2876ca6af070ba3c1f5c5af4ff80dbecbdf3bedf
anthony <anthonyprimacraft@gmail.com>
2019-12-10 22:06:16 -0500

    mapJS and index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test', function(req, res, next) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('/views/index.html', null, function(error, data) {
+    if(error) {
+      response.writeHead(404);
+      response.write("File not found");
+    } else {
+      response.write(data);
+    }
+    response.end();
+
+  })
+});
+
 module.exports = router;
