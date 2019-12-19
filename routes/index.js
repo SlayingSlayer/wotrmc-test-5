5d628a980f27fd2f79935f67e77bd9964e737e38
anthony <anthonyprimacraft@gmail.com>
2019-12-19 18:28:19 -0500

    Version C Mk I

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,43 @@
-var express = require('express');
-var router = express.Router();
+//var express = require('express');
+//var router = express.Router();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/test', function(req, res, next) {
-  onRequest(req, res);
+
+
+var test = 0;
+
+
+
+
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 
 
-function onRequest(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('./index.html', null, function(error, data) {
-    if(error) {
-      response.writeHead(404);
-      response.write("File not found");
-    } else {
-      response.write(data);
-    }
-    response.end();
 
-  })
-}
+// router.get('/test', function(req, res, next) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   fs.readFile('./index.html', null, function(error, data) {
+//     if(error) {
+//       res.writeHead(404);
+//       res.write("File not found");
+//     } else {
+//       res.write(data);
+//     }
+//     res.end();
+
+//   })
+//}
+
 module.exports = router;
+
