9cf33762f2bbb94363da66aa5c0a1c380383e445
anthony <anthonyprimacraft@gmail.com>
2019-12-20 08:49:41 -0500

    Reset

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,28 @@
-//var express = require('express');
-//var router = express.Router();
-
-
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-
-var test = 0;
-
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+router.get('/test', function(req, res, next) {
+  onRequest(req, res);
 });
 
-router.get('/test', function(req, res, next) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+
+
+function onRequest(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
   fs.readFile('./index.html', null, function(error, data) {
     if(error) {
-      res.writeHead(404);
-      res.write("File not found");
+      response.writeHead(404);
+      response.write("File not found");
     } else {
-      res.write(data);
+      response.write(data);
     }
-    res.end();
+    response.end();
 
   })
-});
-
-//module.exports = router;
-
+}
+module.exports = router;
