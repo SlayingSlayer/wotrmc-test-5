ad207bc2da3ea70043670d32231ea442f81868f4
anthony <anthonyprimacraft@gmail.com>
2019-12-20 08:31:59 -0500

    Oldschool

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 //var express = require('express');
 //var router = express.Router();
 
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,33 +13,23 @@ router.get('/', function(req, res, next) {
 var test = 0;
 
 
-
-
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-//module.exports = router;
+router.get('/test', function(req, res, next) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('./index.html', null, function(error, data) {
+    if(error) {
+      res.writeHead(404);
+      res.write("File not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 8080);
+  })
+}
 
-console.log('Running at Port 3000');
-
-// router.get('/test', function(req, res, next) {
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   fs.readFile('./index.html', null, function(error, data) {
-//     if(error) {
-//       res.writeHead(404);
-//       res.write("File not found");
-//     } else {
-//       res.write(data);
-//     }
-//     res.end();
-
-//   })
-//}
-
-
+module.exports = router;
 
